bf0d93387c23777cb5d68d97de4f6e584a594086
JCE000082\aaldayarneh <aaldayarneh@netways.com>
2023-10-29 13:08:22 +0300

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Creating an List<T> of String
+            List<String> firstlist = new List<String>();
+
+            // Adding elements to List
+            firstlist.Add("Geeks");
+            firstlist.Add("For");
+            firstlist.Add("Geeks");
+            firstlist.Add("ajava");
+            firstlist.Add("c#");
+            firstlist.Add("Tutorials");
+            firstlist.Add("GeeksforGeeksjava");
+
+            Console.Write(firstlist.Contains("Java", StringComparer.OrdinalIgnoreCase)); // Should be the whole word
+            //Console.Write(firstlist.Any(a => a.Contains("Java",StringComparison.OrdinalIgnoreCase))); // Should not be the whole word
+        }
+    }
+}
