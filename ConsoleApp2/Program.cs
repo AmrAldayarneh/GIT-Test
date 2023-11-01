0e40e462952a46d10dc7a803f1ea0ffbf71a5699
JCE000082\aaldayarneh <aaldayarneh@netways.com>
2023-11-01 11:57:59 +0300

    New Branch Commit 1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            
+            //new branch 1
            
             List<String> firstlist = new List<String>();
 
